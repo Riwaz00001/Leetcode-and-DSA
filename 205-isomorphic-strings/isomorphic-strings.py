db0bd36743452f184ce66565c95323246e40002b
Riwaz00001 <shresthariwaz77@gmail.com>
2026-08-18 18:51:54 +0545

    Time: 19 ms (9.51%) | Memory: 12.8 MB (96.54%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        if len(s)!=len(t):
+            return False
+        map1=[]
+        map2=[]
+        for i in s:
+            map1.append(s.index(i))
+        for i in t:
+            map2.append(t.index(i))
+        if map1==map2:
+            return True
+        return False
+        
